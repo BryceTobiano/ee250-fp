c0498a79eca7b9c6e66f2c6102979498e5048766
Bryce T <tobiano@usc.edu>
2023-11-27 19:19:02 -0800

    led_test.py: prints brightness

--- rpi/led_test.py
+++ rpi/led_test.py
@@ -8,9 +8,10 @@ analogWrite(led,0)
 if __name__ == '__main__':
     i = 0
     while True: 
-        i = i + 20
+        i = i + 50
         if i > 255:
             i = 0
         analogWrite(led,i)
+        print("Current Brightness: " + i)
         time.sleep(100)
          
